9a13e27f26549f9350d6c26eac1b770985723b13
Jack Cavar <cavarjack@gmail.com>
2024-07-19 11:39:05 +0100

    added files

---
+++ TACO_convert.py
@@ -0,0 +1,39 @@
+import json
+import os
+from shutil import copyfile
+from sklearn.model_selection import train_test_split
+
+def convert_annotation(data, img_dir, label_dir):
+    for item in data['images']:
+        file_name = item['file_name']
+        image_id = item['id']
+        width = item['width']
+        height = item['height']
+        annotations = [a for a in data['annotations'] if a['image_id'] == image_id]
+        label_file_name = file_name.replace('.jpg', '.txt')
+        with open(os.path.join(label_dir, label_file_name), 'w') as f:
+            for a in annotations:
+                category_id = a['category_id'] - 1  # YOLO format requires zero-indexed category ids
+                bbox = a['bbox']
+                x_center = (bbox[0] + bbox[2] / 2) / width
+                y_center = (bbox[1] + bbox[3] / 2) / height
+                bbox_width = bbox[2] / width
+                bbox_height = bbox[3] / height
+                f.write(f"{category_id} {x_center} {y_center} {bbox_width} {bbox_height}\n")
+
+with open('data/annotations.json') as json_file:
+    data = json.load(json_file)
+
+train_data, val_data = train_test_split(data['images'], test_size=0.2, random_state=42)
+
+train_annotations = {'images': train_data, 'annotations': [a for a in data['annotations'] if a['image_id'] in [i['id'] for i in train_data]]}
+val_annotations = {'images': val_data, 'annotations': [a for a in data['annotations'] if a['image_id'] in [i['id'] for i in val_data]]}
+
+convert_annotation(train_annotations, 'data', 'TACO/yolo_format/labels/train')
+convert_annotation(val_annotations, 'data', 'TACO/yolo_format/labels/val')
+
+for item in train_data:
+    copyfile(os.path.join('data', item['file_name']), os.path.join('TACO/yolo_format/images/train', item['file_name']))
+
+for item in val_data:
+    copyfile(os.path.join('data', item['file_name']), os.path.join('TACO/yolo_format/images/val', item['file_name']))
